1320c63a8e924b1816f7d33ad108b3052d68fa22
MacBook <macbook@MacBooks-MacBook-Pro.local>
2021-01-13 15:01:59 +0200

    components Home, Rooms, SingleRoom, Error

---
+++ src/components/Services.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from "react-icons/fa";
+import Title from "./Title";
+
+class Services extends React.Component {
+
+    state = {
+        services: [
+            {
+                icon: <FaCocktail/>,
+                title: "free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna " +
+                    "aliqua."
+            },
+
+            {
+                icon: <FaHiking/>,
+                title: "endless hiking",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna " +
+                    "aliqua."
+            },
+
+            {
+                icon: <FaShuttleVan/>,
+                title: "free shuttle",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna " +
+                    "aliqua."
+            },
+
+            {
+                icon: <FaBeer/>,
+                title: "strongest beer",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna " +
+                    "aliqua."
+            },
+
+        ]
+    }
+
+    render() {
+        return (
+            <section className="services">
+                <Title title="services"/>
+                <div className="services-center">
+                    {
+                        this.state.services.map((item, index) => {
+                            return(
+                                <article key={index} className="service">
+                                    <span>{item.icon}</span>
+                                    <h6>{item.title}</h6>
+                                    <p>{item.info}</p>
+                                </article>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Services;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class SingleRoom extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default SingleRoom;
